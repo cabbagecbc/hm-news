725b8ea55cefef515c84fff255659b88bc526133
白菜 <cabbagecbc@163.com>
2020-08-27 15:10:29 +0800

    配置prettier

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="less">
+<script>
+export default {}
+</script>
+
+<style>
 </style>
